cd41a679a849ddde752e70597a862006a39dd48d
Jyoti <jyoti_kuber@yahoo.com>
2023-03-12 13:39:54 +0000

    Added files
    Added files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Menu from './components/Menu';
+import Cocktail from './components/Cocktail';
+import Timings from './components/Timings';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header/>   
+        <Timings/>
+        <Menu/>
+        <Cocktail/>
+      <Footer/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cocktail.js
@@ -0,0 +1,13 @@
+function Cocktail() {
+  return (
+    <div class="apiContainer">
+        <h3 class="paragraphApi">Want to try our cocktails <span class="atHome"> at home? </span> </h3>
+        <input type="text" placeholder="Introduce name"></input>
+        <button>Show me how</button>
+        <h5 class="name"></h5>
+        <p class="instructions"></p>
+        
+    </div>
+  );
+}
+export default Cocktail;
