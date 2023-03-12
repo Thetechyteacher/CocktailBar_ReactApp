951e5329d0c1e5e7e9be5356c2c7bd94d67c8e3b
Jyoti Kuber <104022136+JyotiKuber@users.noreply.github.com>
2023-03-12 12:51:12 +0000

    added components
    added components folder

---
+++ Menu.js
@@ -0,0 +1,126 @@
+
+function Menu() {
+  return (
+    <section class="padded bg-featured light-text menu grid-center">
+            <div class="col-12 center">
+                <h2 class="text-lg"> menu. </h2>
+            </div>
+           
+            <div class="col-5_lg-6_sm-12 padded-2">
+                <h3> appetisers. </h3>
+                <div class="grid">
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                </div>
+                <h3> tapas. </h3>
+                <div class="grid">
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                </div>
+            </div>
+
+          
+            <div class="col-5_lg-6_sm-12 padded-2 mob-padded-t-0">
+                <h3> classics. </h3>
+                <div class="grid">
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                </div>
+                <h3> negronis. </h3>
+                <div class="grid">
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                    <div class="col-10 editable">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                    </div>
+                    <div class="col-2 editable">
+                        <span class="price">$20 </span>
+                    </div>
+                </div>
+                    <h3> signature. </h3>
+                    <div class="grid">
+                        <div class="col-10 editable">
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        </div>
+                        <div class="col-2 editable">
+                            <span class="price">$20 </span>
+                        </div>
+                        <div class="col-10 editable">
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        </div>
+                        <div class="col-2 editable">
+                            <span class="price">$20 </span>
+                        </div>
+                </div>
+            </div>
+            <div class="col-10_sm-12 padded-2 center">
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, illum, eius excepturi, consectetur vitae molestias voluptates eos deserunt similique maxime minus. Minus, iure? Quia animi vero nisi dolorum odio veniam!</p>
+            </div>
+        </section>
+  );
+}
+export default Menu;
