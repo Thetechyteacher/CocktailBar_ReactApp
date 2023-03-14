90b3cf550c2e524abdb343029f811b59978d9052
Thetechyteacher <thetechyteacherr@gmail.com>
2023-03-14 19:29:41 +0000

    Api and React

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,4 +1,25 @@
-function Cocktail() {
+
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import "../public/index.html";
+
+
+function Cocktail({choice}) {
+  
+  // 
+     
+      useEffect (function() {
+        if (choice){
+            fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`)
+            .then(function (response) {
+              return response.json()
+          })
+        }
+      });
+    
+  
+    
+    
   return (
     <div class="apiContainer">
         <h3 class="paragraphApi">Want to try our cocktails <span class="atHome"> at home? </span> </h3>
@@ -11,3 +32,18 @@ function Cocktail() {
   );
 }
 export default Cocktail;
+
+// return (I though this one would work but it doesn't)
+
+//     GetFetch
+//     ?
+//     <div class="apiContainer">
+//       <h3 class="paragraphApi">Want to try our cocktails <span class="atHome"> at home? </span> </h3>
+//      <input type="text" placeholder="Introduce name"/>
+//       <button>Show me how</button>
+//       <h5 class="name"> {data.drinks[0].strDrink}</h5>
+//       <p class="instructions"> {data.drinks[0].strInstructions} </p>
+//       </div>
+// :
+
+// <h3>Data will appear here</h3>
--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,41 +1,42 @@
-import React, { useState, useEffect } from "react"
-import "./App.css"
-import "src/index.html"
-
-function getData({choice}){
+// import React, { useState, useEffect } from "react"
+// import "./App.css"
+// import "src/index.html"
 
 
-const [input, setInput] = useState()
+// function GetFetch({choice}){
+
+
+// const [input, setInput] = useState()
 
  
-  useEffect (function() {
-    if (choice){
-        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`)
-        .then(function (response) {
-          return response.json()
-      })
-    }
-  });
+//   useEffect (function() {
+//     if (choice){
+//         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`)
+//         .then(function (response) {
+//           return response.json()
+//       })
+//     }
+//   });
 
-return (
+// return (
 
-    getData
-    ?
-    <div class="apiContainer">
-      <h3 class="paragraphApi">Want to try our cocktails <span class="atHome"> at home? </span> </h3>
-     <input type="text" placeholder="Introduce name"/>
-      <button>Show me how</button>
-      <h5 class="name"> {data.drinks[0].strDrink}</h5>
-      <p class="instructions"> {data.drinks[0].strInstructions} </p>
-      </div>
-:
+//     GetFetch
+//     ?
+//     <div class="apiContainer">
+//       <h3 class="paragraphApi">Want to try our cocktails <span class="atHome"> at home? </span> </h3>
+//      <input type="text" placeholder="Introduce name"/>
+//       <button>Show me how</button>
+//       <h5 class="name"> {data.drinks[0].strDrink}</h5>
+//       <p class="instructions"> {data.drinks[0].strInstructions} </p>
+//       </div>
+// :
 
-<h3>Data will appear here</h3>
+// <h3>Data will appear here</h3>
 
-)}
+// )}
      
 
-      export default getData;
+//       export default GetFetch; 
 
       
  
