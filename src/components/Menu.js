4181e4eb6cb6dc6966c80dfd2a975d15db1316af
Jyoti <jyoti_kuber@yahoo.com>
2023-03-18 15:23:01 +0000

    added menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,55 +10,55 @@ function Menu() {
                 <h3> appetisers. </h3>
                 <div class="grid">
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Winter greens, cucumber, roasted pumpkin, hummus, mixed seeds, buckwheat, sauerkraut, pickled carrot & puffed rice. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$8.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Risotto with crispy enoki & parmesan cream. Chicken breast with artichoke purée, wild mushrooms & spinach. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$9.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Chestnut Hummus Pickled red onions, celery & wholewheat roti. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$6.50 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Shrimp Lolipops Vegan shrimp & sweet chilli dipping sauce. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$6.95 </span>
                     </div>
                 </div>
                 <h3> tapas. </h3>
                 <div class="grid">
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Wild new forest mushroom soup, beef fat fried brioche, quail egg, roasted selection of mushrooms. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$17.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Beetroot cured Loch Duart salmon, yuzu purée, pickled heritage beetroot, red meat radishes furikake,Japanese dressing. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$19.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Berners Tavern house salad Salt beef, poached egg, sundried tomatoes French beans, ranch dressing. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$15.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Berners Tavern mac and cheese with braised beef blade or poached lobster. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$19.95 </span>
                     </div>
                 </div>
             </div>
@@ -68,57 +68,58 @@ function Menu() {
                 <h3> classics. </h3>
                 <div class="grid">
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Pumpkin & Sage Raviolli Vegan ‘Nduja cream, “feta”, baby spinach & pumpkin seeds. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$16.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Roast Chicken Breast Artichoke purée, wild mushrooms & spinach. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$16.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Fish & Chips Beer batter, mushy peas & tartare sauce. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$14.95 </span>
                     </div>
                 </div>
                 <h3> negronis. </h3>
                 <div class="grid">
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Flash Grilled Mini Steak Chips, watercress & Béarnaise sauce. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$17.95 </span>
                     </div>
                     <div class="col-10 editable">
-                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                        <p> Wild Mashroom Risotto Crispy enoki & parmesan cream. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$20 </span>
+                        <span class="price">$14.95 </span>
                     </div>
                 </div>
                     <h3> signature. </h3>
                     <div class="grid">
                         <div class="col-10 editable">
-                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                            <p> Mezze Board Chestnut hummus, harissa, burella (vegan buratta), flatbread & marinated beets. </p>
                         </div>
                         <div class="col-2 editable">
-                            <span class="price">$20 </span>
+                            <span class="price">$19.95 </span>
                         </div>
                         <div class="col-10 editable">
-                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequuntur sequi qui. Accusantium at excepturi. </p>
+                            <p>  </p>Charcuterie Board Salami, chorizo, prosciutto, Manchego, sourdough, apple & fig chutney
                         </div>
                         <div class="col-2 editable">
-                            <span class="price">$20 </span>
+                            <span class="price">$21.95 </span>
                         </div>
                 </div>
             </div>
             <div class="col-10_sm-12 padded-2 center">
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, illum, eius excepturi, consectetur vitae molestias voluptates eos deserunt similique maxime minus. Minus, iure? Quia animi vero nisi dolorum odio veniam!</p>
+                <p> Bringing cocktails, live music and a touch of old school glamour to Scotland for over 10 years!</p>
+                <p>The Negroni Cocktail Bar operates a no reservation policy.</p>
             </div>
         </section>
   );
