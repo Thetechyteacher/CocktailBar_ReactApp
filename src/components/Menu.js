dddf3cfe70bd74d9f008b3e32f208208483f2b86
Monica <117657937+Thetechyteacher@users.noreply.github.com>
2023-03-18 19:18:44 +0100

    changed dollar sign and modified the style of the API section

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,25 +13,25 @@ function Menu() {
                         <p> Winter greens, cucumber, roasted pumpkin, hummus, mixed seeds, buckwheat, sauerkraut, pickled carrot & puffed rice. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$8.95 </span>
+                        <span class="price">£8.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Risotto with crispy enoki & parmesan cream. Chicken breast with artichoke purée, wild mushrooms & spinach. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$9.95 </span>
+                        <span class="price">£9.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Chestnut Hummus Pickled red onions, celery & wholewheat roti. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$6.50 </span>
+                        <span class="price">£6.50 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Shrimp Lolipops Vegan shrimp & sweet chilli dipping sauce. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$6.95 </span>
+                        <span class="price">£6.95 </span>
                     </div>
                 </div>
                 <h3> tapas. </h3>
@@ -40,25 +40,25 @@ function Menu() {
                         <p> Wild new forest mushroom soup, beef fat fried brioche, quail egg, roasted selection of mushrooms. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$17.95 </span>
+                        <span class="price">£17.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Beetroot cured Loch Duart salmon, yuzu purée, pickled heritage beetroot, red meat radishes furikake,Japanese dressing. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$19.95 </span>
+                        <span class="price">£19.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Berners Tavern house salad Salt beef, poached egg, sundried tomatoes French beans, ranch dressing. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$15.95 </span>
+                        <span class="price">£15.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Berners Tavern mac and cheese with braised beef blade or poached lobster. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$19.95 </span>
+                        <span class="price">£19.95 </span>
                     </div>
                 </div>
             </div>
@@ -71,19 +71,19 @@ function Menu() {
                         <p> Pumpkin & Sage Raviolli Vegan ‘Nduja cream, “feta”, baby spinach & pumpkin seeds. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$16.95 </span>
+                        <span class="price">£16.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Roast Chicken Breast Artichoke purée, wild mushrooms & spinach. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$16.95 </span>
+                        <span class="price">£16.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Fish & Chips Beer batter, mushy peas & tartare sauce. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$14.95 </span>
+                        <span class="price">£14.95 </span>
                     </div>
                 </div>
                 <h3> negronis. </h3>
@@ -92,13 +92,13 @@ function Menu() {
                         <p> Flash Grilled Mini Steak Chips, watercress & Béarnaise sauce. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$17.95 </span>
+                        <span class="price">£17.95 </span>
                     </div>
                     <div class="col-10 editable">
                         <p> Wild Mashroom Risotto Crispy enoki & parmesan cream. </p>
                     </div>
                     <div class="col-2 editable">
-                        <span class="price">$14.95 </span>
+                        <span class="price">£14.95 </span>
                     </div>
                 </div>
                     <h3> signature. </h3>
@@ -107,13 +107,13 @@ function Menu() {
                             <p> Mezze Board Chestnut hummus, harissa, burella (vegan buratta), flatbread & marinated beets. </p>
                         </div>
                         <div class="col-2 editable">
-                            <span class="price">$19.95 </span>
+                            <span class="price">£19.95 </span>
                         </div>
                         <div class="col-10 editable">
                             <p>  </p>Charcuterie Board Salami, chorizo, prosciutto, Manchego, sourdough, apple & fig chutney
                         </div>
                         <div class="col-2 editable">
-                            <span class="price">$21.95 </span>
+                            <span class="price">£21.95 </span>
                         </div>
                 </div>
             </div>
